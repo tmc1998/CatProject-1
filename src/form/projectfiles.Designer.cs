5caea73ce4c817da923687a38efb5a76a18464dc
Nguyen Thanh Long <ntlong281098@gmail.com>
2020-03-20 21:35:48 +0700

    Commic form projectfile

--- src/form/projectfiles.Designer.cs
+++ src/form/projectfiles.Designer.cs
@@ -41,6 +41,10 @@
             this.lblCreationTime = new System.Windows.Forms.Label();
             this.lbltimecreation = new System.Windows.Forms.Label();
             this.lblLocationFile = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // listViewFiles
@@ -50,9 +54,9 @@
             this.listViewFiles.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.listViewFiles.HideSelection = false;
             this.listViewFiles.LargeImageList = this.imageListIcon;
-            this.listViewFiles.Location = new System.Drawing.Point(13, 13);
+            this.listViewFiles.Location = new System.Drawing.Point(8, 12);
             this.listViewFiles.Name = "listViewFiles";
-            this.listViewFiles.Size = new System.Drawing.Size(481, 564);
+            this.listViewFiles.Size = new System.Drawing.Size(481, 548);
             this.listViewFiles.SmallImageList = this.imageListIcon;
             this.listViewFiles.TabIndex = 0;
             this.listViewFiles.UseCompatibleStateImageBehavior = false;
@@ -67,7 +71,7 @@
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(10, 592);
+            this.lbl1.Location = new System.Drawing.Point(3, 13);
             this.lbl1.Name = "lbl1";
             this.lbl1.Size = new System.Drawing.Size(65, 13);
             this.lbl1.TabIndex = 1;
@@ -76,7 +80,7 @@
             // lbl2
             // 
             this.lbl2.AutoSize = true;
-            this.lbl2.Location = new System.Drawing.Point(10, 638);
+            this.lbl2.Location = new System.Drawing.Point(3, 57);
             this.lbl2.Name = "lbl2";
             this.lbl2.Size = new System.Drawing.Size(81, 13);
             this.lbl2.TabIndex = 2;
@@ -84,7 +88,7 @@
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(511, 663);
+            this.btnOk.Location = new System.Drawing.Point(533, 71);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 3;
@@ -94,7 +98,7 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(419, 663);
+            this.btnCancel.Location = new System.Drawing.Point(452, 71);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 4;
@@ -104,7 +108,7 @@
             // 
             // btnHorizontal
             // 
-            this.btnHorizontal.Location = new System.Drawing.Point(511, 212);
+            this.btnHorizontal.Location = new System.Drawing.Point(519, 206);
             this.btnHorizontal.Name = "btnHorizontal";
             this.btnHorizontal.Size = new System.Drawing.Size(89, 23);
             this.btnHorizontal.TabIndex = 5;
@@ -114,7 +118,7 @@
             // 
             // btnVertical
             // 
-            this.btnVertical.Location = new System.Drawing.Point(511, 257);
+            this.btnVertical.Location = new System.Drawing.Point(519, 255);
             this.btnVertical.Name = "btnVertical";
             this.btnVertical.Size = new System.Drawing.Size(89, 23);
             this.btnVertical.TabIndex = 6;
@@ -125,7 +129,7 @@
             // lblNameProject
             // 
             this.lblNameProject.AutoSize = true;
-            this.lblNameProject.Location = new System.Drawing.Point(98, 592);
+            this.lblNameProject.Location = new System.Drawing.Point(85, 13);
             this.lblNameProject.Name = "lblNameProject";
             this.lblNameProject.Size = new System.Drawing.Size(35, 13);
             this.lblNameProject.TabIndex = 7;
@@ -134,7 +138,7 @@
             // lblCreationTime
             // 
             this.lblCreationTime.AutoSize = true;
-            this.lblCreationTime.Location = new System.Drawing.Point(10, 614);
+            this.lblCreationTime.Location = new System.Drawing.Point(3, 35);
             this.lblCreationTime.Name = "lblCreationTime";
             this.lblCreationTime.Size = new System.Drawing.Size(75, 13);
             this.lblCreationTime.TabIndex = 8;
@@ -143,7 +147,7 @@
             // lbltimecreation
             // 
             this.lbltimecreation.AutoSize = true;
-            this.lbltimecreation.Location = new System.Drawing.Point(98, 614);
+            this.lbltimecreation.Location = new System.Drawing.Point(85, 35);
             this.lbltimecreation.Name = "lbltimecreation";
             this.lbltimecreation.Size = new System.Drawing.Size(35, 13);
             this.lbltimecreation.TabIndex = 9;
@@ -152,35 +156,55 @@
             // lblLocationFile
             // 
             this.lblLocationFile.AutoSize = true;
-            this.lblLocationFile.Location = new System.Drawing.Point(98, 638);
+            this.lblLocationFile.Location = new System.Drawing.Point(85, 57);
             this.lblLocationFile.Name = "lblLocationFile";
             this.lblLocationFile.Size = new System.Drawing.Size(35, 13);
             this.lblLocationFile.TabIndex = 10;
             this.lblLocationFile.Text = "label1";
             // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Controls.Add(this.lblLocationFile);
+            this.panel1.Controls.Add(this.lblNameProject);
+            this.panel1.Controls.Add(this.btnOk);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.lbltimecreation);
+            this.panel1.Controls.Add(this.lblCreationTime);
+            this.panel1.Controls.Add(this.lbl2);
+            this.panel1.Location = new System.Drawing.Point(6, 593);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(624, 100);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.listViewFiles);
+            this.panel2.Controls.Add(this.btnHorizontal);
+            this.panel2.Controls.Add(this.btnVertical);
+            this.panel2.Location = new System.Drawing.Point(6, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(624, 583);
+            this.panel2.TabIndex = 12;
+            // 
             // projectfiles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 689);
-            this.Controls.Add(this.lblLocationFile);
-            this.Controls.Add(this.lbltimecreation);
-            this.Controls.Add(this.lblCreationTime);
-            this.Controls.Add(this.lblNameProject);
-            this.Controls.Add(this.btnVertical);
-            this.Controls.Add(this.btnHorizontal);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnOk);
-            this.Controls.Add(this.lbl2);
-            this.Controls.Add(this.lbl1);
-            this.Controls.Add(this.listViewFiles);
+            this.ClientSize = new System.Drawing.Size(636, 699);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "projectfiles";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Project Files";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.projectfiles_FormClosing);
             this.Load += new System.EventHandler(this.projectfiles_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -198,5 +222,7 @@
         private System.Windows.Forms.Label lblCreationTime;
         private System.Windows.Forms.Label lbltimecreation;
         private System.Windows.Forms.Label lblLocationFile;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
